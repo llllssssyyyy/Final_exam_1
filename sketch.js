273b00d7f72cf9884f09278e79ef802b03d668ca
llllssssyyyy <iamsyl0831@naver.com>
2023-11-25 12:29:13 +0900

    Repeller 추가
    방해되는 attractor3은 삭제함

--- sketch.js
+++ sketch.js
@@ -3,17 +3,17 @@ let emitter;
 
 //{!1} One repeller
 let repeller;
+let repeller2
 let att;
 let att2;
-let att3
 
 function setup() {
   createCanvas(400, 400);
   emitter = new Emitter(width / 2, height/2);
-  repeller = new Repeller(width / 2, 350);
-  att = new Attractor(20, 380);
-  att2 = new Attractor(380, 380);
-  att3 = new Attractor(200, 20)
+  repeller = new Repeller(width / 4, 350);
+  repeller2 = new Repeller(width*3 / 4, 350);
+  att = new Attractor(20, height/2);
+  att2 = new Attractor(380, height/2);
 }
 
 function draw() {
@@ -21,6 +21,9 @@ function draw() {
     
   repeller.setPower(mouseX);
   repeller.move(0.5);
+
+  repeller2.setPower(mouseX);
+  repeller2.move(0.5);
   
   emitter.addParticle();
   emitter.addParticle();
@@ -31,13 +34,13 @@ function draw() {
   emitter.applyForce(gravity);
   //{!1} Applying the repeller
   emitter.applyRepeller(repeller);
+  emitter.applyRepeller(repeller);
   emitter.applyAttractor(att);
   emitter.applyAttractor(att2);
-  emitter.applyAttractor(att3);
   emitter.run();
 
   repeller.show();
+  repeller2.show();
   att.show();
   att2.show();
-  att3.show();
 }

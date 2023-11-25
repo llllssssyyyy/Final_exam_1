df626f98b45935e65af9e33d16f26a9c7eb265ca
llllssssyyyy <iamsyl0831@naver.com>
2023-11-25 09:36:12 +0900

    Attractor 추가

--- sketch.js
+++ sketch.js
@@ -4,12 +4,14 @@ let emitter;
 //{!1} One repeller
 let repeller;
 let att;
+let att2;
 
 function setup() {
   createCanvas(400, 400);
   emitter = new Emitter(width / 2, height/2);
   repeller = new Repeller(width / 2, 350);
   att = new Attractor(20, height/2);
+  att2 = new Attractor2(200,height/2);
 }
 
 function draw() {
@@ -28,8 +30,10 @@ function draw() {
   //{!1} Applying the repeller
   emitter.applyRepeller(repeller);
   emitter.applyAttractor(att);
+  emitter.applyAtrractor(att2);
   emitter.run();
 
   repeller.show();
   att.show();
+  att2.show();
 }
